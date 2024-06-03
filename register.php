e50bbd99f59ffeef470568c767d2c4ccf8156090
POFFIOLEG <144817552+rngblessed@users.noreply.github.com>
2024-06-04 00:31:52 +0300

    12341234

--- register.php
+++ register.php
@@ -4,8 +4,5 @@ $login = $_POST['login'];
 $tel = $_POST['tel'];
 $email = $_POST['email'];
 $date = $_POST['date'];
-// $someGroupName = $_POST['someGroupName'];
-// $language[] = $_POST['language'];
 $bio = $_POST['bio'];
-// $checkt = $_POST['checkt'];
 ?>
